a4b51a23481e28aeb070680a903edae40371025a
Natalia Quiroga <nataliaquirogag@gmail.com>
2022-11-01 02:14:55 -0400

    primerbloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:trip_planner/pages/home.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trip_planner/blocs/blocs.dart';
 import 'package:trip_planner/pages/recorrido_lineas.dart';
+import 'package:trip_planner/screens/loading_screen.dart';
 
-void main() => runApp(const MainClass());
+void main() {
+  runApp(MultiBlocProvider(
+    providers: [BlocProvider(create: (context) => GpsBloc())],
+    child: const MainClass(),
+  ));
+}
 
 class MainClass extends StatelessWidget {
   const MainClass({super.key});
@@ -14,8 +21,8 @@ class MainClass extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: 'home',
       routes: {
-        'home': (_) => HomePage(),
-        'recorrido_lineas': (_) => RecorridoLineas(),
+        'home': (_) => LoadingScreen(),
+        'recorrido_lineas': (_) => RecorridoLineas()
       },
     );
   }
