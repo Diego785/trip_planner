f6ad1af30dfdb02bb61811c583aeb5c1812725ed
Diego Hurtado Vargas <diegohurtado2412@gmail.com>
2022-12-10 16:31:18 -0400

    searcher in server

--- lib/services/puntos_services.dart
+++ lib/services/puntos_services.dart
@@ -8,7 +8,7 @@ class PuntosService {
     var params = {"recorrido": recorrido.toString(), "par": par.toString()};
     final urlPrincipal = ServerProvider().url;
     final url = Uri.parse('$urlPrincipal/api/punto');
-    // final url =  Uri.parse("http://10.0.2.2/trip_planner_bd/public/api/punto");
+    //final url =  Uri.parse("http://10.0.2.2/trip_planner_bd/public/api/punto");
     final urlP = url.replace(queryParameters: params);
     final response = await http.get(urlP, headers: {
       'Content-Type': 'application/json',
